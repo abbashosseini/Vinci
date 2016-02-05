e176d3a84702a000d19c42c41047d5500a0fadc5
abbashosseini <abbashosseini@mail.com>
2016-02-05 22:23:32 +0330

    added download image and put it in internal storage

--- src/main/java/mklib/hosseini/com/vinci/Main/PhotoProcess.java
+++ src/main/java/mklib/hosseini/com/vinci/Main/PhotoProcess.java
@@ -2,6 +2,9 @@ package mklib.hosseini.com.vinci.Main;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -17,15 +20,14 @@ import mklib.hosseini.com.vinci.list.ImmutableList;
  */
 public class PhotoProcess {
 
-    private static URL url;
+    private static URL url = null;
 
     public static class logger implements Callable<byte[]> {
 
         final ImmutableList<byte[]> messages = new ImmutableList<>();
 
-        public logger(String url){
-
-            log(BackgroundProcess(url));
+        public logger(final String url){
+            log(load(url));
         }
 
         public void log(byte[] message) {
@@ -38,11 +40,10 @@ public class PhotoProcess {
         }
     }
 
-    public static synchronized byte[] BackgroundProcess(String url) {
+    public static synchronized byte[] load(final String url) {
 
         Bitmap bitmap;
-        try {
-                /*
+        try {  /*
                 * get URL for downloading image from internet and convert it to Byte ARRAY
                 * and put it in BLOB field ( database ).
                 */
@@ -66,4 +67,26 @@ public class PhotoProcess {
             }
 
         }
+
+    public synchronized static Bitmap drawableToBitmap (Drawable drawable) {
+        Bitmap bitmap;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if(bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
     }
--- src/main/java/mklib/hosseini/com/vinci/Main/Vinci.java
+++ src/main/java/mklib/hosseini/com/vinci/Main/Vinci.java
@@ -3,16 +3,24 @@ package mklib.hosseini.com.vinci.Main;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Path;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Environment;
+
 
 import java.io.ByteArrayInputStream;
-import java.util.Arrays;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Vinci {
 
@@ -22,16 +30,75 @@ public class Vinci {
         Vinci.context = context;
     }
 
-    public synchronized Drawable PhotoFromDB(byte[] imageByte){
+
+    public synchronized Drawable AndDrawable(byte[] imageByte){
 
         ByteArrayInputStream imageStream = new ByteArrayInputStream(imageByte);
         Bitmap image = BitmapFactory.decodeStream(imageStream);
-
         return new BitmapDrawable(context.getResources(), image);
     }
 
-    public synchronized byte[] PhotoInDB(String Url) throws Exception {
+    public boolean intoStorage(Bitmap bitmapImage, String name) throws IOException {
 
+        /* parse string and extract name with extension from String*/
+        String fileName = FileName(name);
+
+        /*
+        * compress bitmap to real size (100) if you use less then 100,  image
+        * converter to Lighter then  real size  */
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+
+        /*
+        * setup path in device for save image file to it and its defualt path is :
+        * /storage/emulated/0
+        * and full path is :
+        * /storage/emulated/0/Vinci/Pictures */
+        String RootFolder = Environment.getExternalStorageDirectory() + File.separator
+                          +String.format("%s/%s",
+                                getClass().getSimpleName(),
+                                "Pictures");
+
+        File path = new File(RootFolder);
+        boolean DirectorIsCreated = path.mkdirs();
+
+        File fileImage = new File(path, fileName);
+        boolean FileIsCrreated = fileImage.createNewFile();
+
+        if (!DirectorIsCreated)
+            throw new IllegalAccessError("director cant be created !");
+
+        if (!FileIsCrreated)
+            throw new IllegalAccessError("File cant be created !");
+
+        //write the bytes in file
+        FileOutputStream fo = new FileOutputStream(fileImage);
+        fo.write(bytes.toByteArray());
+
+        // remember close de FileOutput
+        fo.close();
+
+        return true;
+    }
+
+    public String FileName(String name){
+        Pattern pattern = Pattern.compile("([\\w]+.\\w{3})$");
+        Matcher matcher = pattern.matcher(name);
+
+        if (!matcher.find())
+            throw new IllegalArgumentException("Name File not valid !");
+
+        return matcher.group();
+    }
+
+    public synchronized Bitmap AndBitmap(String Url) throws InterruptedException, ExecutionException {
+
+        ByteArrayInputStream imageStream = new ByteArrayInputStream(AndByte(Url));
+        return BitmapFactory.decodeStream(imageStream);
+
+    }
+
+    public synchronized byte[] AndByte(String Url) throws InterruptedException, ExecutionException {
 
         ExecutorService service =  Executors.newSingleThreadExecutor();
         PhotoProcess.logger sumTask = new PhotoProcess.logger(Url);
